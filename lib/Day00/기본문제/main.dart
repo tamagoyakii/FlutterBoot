5999f5f0c7ed0aa5ab7965bfa0b928ce2232cb3d
tamagoyakii <rlawlgus4063@naver.com>
2023-11-27 00:04:12 +0900

    [format] 콤마 추가

--- lib/Day00/기본문제/main.dart
+++ lib/Day00/기본문제/main.dart
@@ -37,58 +37,59 @@ class HelloFlutterState extends State<HelloFlutter> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          leading: const Icon(Icons.home),
-          title: const Text('Hello Flutter'),
-          actions: [
-            Container(
-              margin: const EdgeInsets.fromLTRB(0, 0, 15, 0),
-              child: const Icon(Icons.help),
-            )
+      appBar: AppBar(
+        leading: const Icon(Icons.home),
+        title: const Text('Hello Flutter'),
+        actions: [
+          Container(
+            margin: const EdgeInsets.fromLTRB(0, 0, 15, 0),
+            child: const Icon(Icons.help),
+          )
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Your score',
+              style: TextStyle(
+                fontWeight: FontWeight.w900,
+                fontSize: 25,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              '$score',
+              style: const TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                FilledButton(
+                  onPressed: increaseScore,
+                  child: const Icon(
+                    Icons.add,
+                    size: 15,
+                  ),
+                ),
+                const SizedBox(width: 15),
+                FilledButton(
+                  onPressed: decreaseScore,
+                  child: const Icon(
+                    Icons.remove,
+                    size: 15,
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'Your score',
-                style: TextStyle(
-                  fontWeight: FontWeight.w900,
-                  fontSize: 25,
-                ),
-              ),
-              const SizedBox(height: 10),
-              Text(
-                '$score',
-                style: const TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              const SizedBox(height: 10),
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  FilledButton(
-                    onPressed: increaseScore,
-                    child: const Icon(
-                      Icons.add,
-                      size: 15,
-                    ),
-                  ),
-                  const SizedBox(width: 15),
-                  FilledButton(
-                    onPressed: decreaseScore,
-                    child: const Icon(
-                      Icons.remove,
-                      size: 15,
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ));
+      ),
+    );
   }
 }
