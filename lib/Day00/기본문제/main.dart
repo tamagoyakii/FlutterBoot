ae53a67662f777b2bfcc598616347b3bfac4e09c
tamagoyakii <rlawlgus4063@naver.com>
2023-11-26 23:43:43 +0900

    [feat] HelloFlutter

--- lib/Day00/기본문제/main.dart
+++ lib/Day00/기본문제/main.dart
@@ -37,81 +37,58 @@ class HelloFlutterState extends State<HelloFlutter> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: Icon(Icons.home),
-        title: Text('Hello Flutter'),
-        actions: [
-          Container(
-            margin: EdgeInsets.fromLTRB(0, 0, 15, 0),
-            child: Icon(Icons.help),
-          )
-        ],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Your score',
-              style: TextStyle(
-                fontWeight: FontWeight.w900,
-                fontSize: 25,
-              ),
-            ),
-            Text(
-              '$score',
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
+        appBar: AppBar(
+          leading: Icon(Icons.home),
+          title: Text('Hello Flutter'),
+          actions: [
             Container(
-              width: 130,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              margin: EdgeInsets.fromLTRB(0, 0, 15, 0),
+              child: Icon(Icons.help),
+            )
+          ],
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Your score',
+                style: TextStyle(
+                  fontWeight: FontWeight.w900,
+                  fontSize: 25,
+                ),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                '$score',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
-                  Container(
-                    width: 60,
-                    height: 30,
-                    child: FilledButton(
-                      style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStatePropertyAll<Color>(Colors.deepPurple),
-                      ),
-                      onPressed: increaseScore,
-                      child: Text(
-                        '+',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
+                  FilledButton(
+                    onPressed: increaseScore,
+                    child: const Icon(
+                      Icons.add,
+                      size: 15,
                     ),
                   ),
-                  Container(
-                    width: 60,
-                    height: 30,
-                    child: FilledButton(
-                      style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStatePropertyAll<Color>(Colors.deepPurple),
-                      ),
-                      onPressed: decreaseScore,
-                      child: Text(
-                        '-',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
+                  const SizedBox(width: 15),
+                  FilledButton(
+                    onPressed: decreaseScore,
+                    child: const Icon(
+                      Icons.remove,
+                      size: 15,
                     ),
                   ),
                 ],
               ),
-            )
-          ],
-        ),
-      ),
-    );
+            ],
+          ),
+        ));
   }
 }
