a470b1a8988a66717a87cbec290bac82052f4cdb
tamagoyakii <rlawlgus4063@naver.com>
2023-11-28 01:19:02 +0900

    [feat] key

--- lib/Day00/기본문제/HelloFlutter.dart
+++ lib/Day00/기본문제/HelloFlutter.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HelloFlutter(),
     );
@@ -15,6 +17,8 @@ class MyApp extends StatelessWidget {
 }
 
 class HelloFlutter extends StatefulWidget {
+  const HelloFlutter({super.key});
+
   @override
   State<HelloFlutter> createState() => HelloFlutterState();
 }
