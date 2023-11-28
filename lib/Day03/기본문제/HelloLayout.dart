6e039ed17d3fe78cff4758854a3ae6955b1f5504
tamagoyakii <rlawlgus4063@naver.com>
2023-11-29 01:26:08 +0900

    [feat] HelloLayout

---
+++ lib/Day03/기본문제/HelloLayout.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'HelloLayout',
+      debugShowCheckedModeBanner: false,
+      home: HelloLayout(),
+    );
+  }
+}
+
+class HelloLayout extends StatelessWidget {
+  const HelloLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        title: const Text(
+          'I can layout this',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            LayoutBuilder(
+              builder: (context, constraints) {
+                double width = constraints.maxWidth;
+
+                return Container(
+                  height: width,
+                  decoration: const BoxDecoration(
+                    color: Colors.black,
+                  ),
+                  child: GridView(
+                    shrinkWrap: true,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisSpacing: 5,
+                      mainAxisSpacing: 5,
+                      crossAxisCount: 2,
+                    ),
+                    children: [
+                      Container(color: Colors.green),
+                      Container(
+                        color: Colors.orange,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Container(
+                              height: width * 0.25,
+                              color: Colors.white,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        color: Colors.blue,
+                        child: Column(
+                          children: [
+                            Container(
+                              height: width * 0.325,
+                              color: Colors.white,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        color: Colors.white,
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(15),
+                              child: Container(
+                                height: width * 0.25,
+                                color: Colors.red,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+            Expanded(
+              child: LayoutBuilder(
+                builder: (context, constraints) {
+                  double height = constraints.maxHeight;
+
+                  return Column(
+                    children: [
+                      Container(
+                        height: height * 0.7,
+                        decoration: const BoxDecoration(
+                          color: Colors.purple,
+                        ),
+                      ),
+                      Container(
+                        height: height * 0.3,
+                        decoration: const BoxDecoration(
+                          color: Colors.green,
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
