482f4458970277c9be90700c83ff14aad6af1466
tamagoyakii <rlawlgus4063@naver.com>
2023-12-01 01:35:13 +0900

    [feat] HelloListView

---
+++ lib/Day05/기본문제/HelloListView.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+
+final spaceData = {
+  'NGC 162': 1862,
+  '87 Sylvia': 1866,
+  'R 136a1': 1985,
+  '28978 Ixion': 2001,
+  'NGC 6715': 1778,
+  '94400 Hongdaeyong': 2001,
+  '6354 Vangelis': 1934,
+  'C/2020 F3': 2020,
+  'Cartwheel Galaxy': 1941,
+  'Sculptor Dwarf Elliptical Galaxy': 1937,
+  'Eight-Burst Nebula': 1835,
+  'Rhea': 1672,
+  'C/1702 H1': 1702,
+  'Messier 5': 1702,
+  'Messier 50': 1711,
+  'Cassiopeia A': 1680,
+  'Great Comet of 1680': 1680,
+  'Butterfly Cluster': 1654,
+  'Triangulum Galaxy': 1654,
+  'Comet of 1729': 1729,
+  'Omega Nebula': 1745,
+  'Eagle Nebula': 1745,
+  'Small Sagittarius Star Cloud': 1764,
+  'Dumbbell Nebula': 1764,
+  '54509 YORP': 2000,
+  'Dia': 2000,
+  '63145 Choemuseon': 2000,
+};
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'HelloLayout',
+      debugShowCheckedModeBanner: false,
+      home: HelloListView(),
+    );
+  }
+}
+
+class HelloListView extends StatefulWidget {
+  const HelloListView({super.key});
+
+  @override
+  State<HelloListView> createState() => _HelloListViewState();
+}
+
+class _HelloListViewState extends State<HelloListView> {
+  final List _spaceDataKeys = spaceData.keys.toList();
+  final ScrollController _scrollController = ScrollController();
+
+  void _scrollToTop() {
+    _scrollController.animateTo(
+      0.0,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeInOut,
+    );
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  Widget _buildListView() {
+    return ListView.separated(
+      controller: _scrollController,
+      padding: const EdgeInsets.all(12),
+      separatorBuilder: (BuildContext context, int index) =>
+          const SizedBox(height: 12),
+      itemCount: spaceData.length,
+      itemBuilder: (context, index) => _buildListItem(index),
+    );
+  }
+
+  Widget _buildListItem(int index) {
+    return Container(
+      padding: const EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey, width: 1.0),
+        borderRadius: BorderRadius.circular(3),
+      ),
+      child: Row(
+        children: [
+          const Icon(Icons.travel_explore),
+          const SizedBox(width: 10),
+          Text(
+            '${_spaceDataKeys[index]} was discovered in ${spaceData[_spaceDataKeys[index]]}',
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('My First ListView!')),
+      body: SafeArea(child: _buildListView()),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _scrollToTop,
+        child: const Icon(Icons.keyboard_arrow_up),
+      ),
+    );
+  }
+}
