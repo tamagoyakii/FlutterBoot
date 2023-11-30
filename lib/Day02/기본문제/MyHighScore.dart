1f5dabcf16c992beb6723a528d7445166ec23b10
tamagoyakii <rlawlgus4063@naver.com>
2023-12-01 01:41:54 +0900

    [fix] 돌아온 ValueNotifier & ValueListenableBuilder in flutter

--- lib/Day02/기본문제/MyHighScore.dart
+++ lib/Day02/기본문제/MyHighScore.dart
@@ -26,9 +26,9 @@ class MyHighScore extends StatefulWidget {
 }
 
 class _MyHighScoreState extends State<MyHighScore> {
-  double progressValue = 0.0;
+  ValueNotifier<double> gauge = ValueNotifier(0.0);
+  Timer? timer;
   int score = 0;
-  late Timer timer;
 
   void addScore() {
     setState(() {
@@ -39,80 +39,94 @@ class _MyHighScoreState extends State<MyHighScore> {
   void resetScore() {
     setState(() {
       score = 0;
-      progressValue = 0.0;
+      gauge.value = 0;
     });
   }
 
   void onButtonPress() {
+    setTimer();
     setState(() {
-      if (progressValue < 1) {
-        progressValue += 0.4;
+      if (gauge.value < 1) {
+        gauge.value += 0.4;
       }
-      if (progressValue >= 1) {
+      if (gauge.value >= 1) {
         addScore();
       }
     });
   }
 
+  void setTimer() {
+    if (timer?.isActive != true) {
+      timer = Timer.periodic(const Duration(milliseconds: 15), (timer) {
+        if (gauge.value > 0) {
+          gauge.value -= 0.03;
+        }
+      });
+    }
+  }
+
   @override
   void initState() {
     super.initState();
-    timer = Timer.periodic(const Duration(milliseconds: 15), (timer) {
-      if (progressValue > 0) {
-        setState(() {
-          progressValue -= 0.03;
-        });
+    gauge.addListener(() {
+      if (gauge.value <= 0) {
+        resetScore();
+        timer?.cancel();
       }
     });
   }
 
   @override
   void dispose() {
-    timer.cancel();
     super.dispose();
+    gauge.dispose();
+    timer?.cancel();
   }
 
-  Widget _buildProgressBar() {
-    return Align(
-      alignment: Alignment.bottomRight,
-      child: LayoutBuilder(
-        builder: (context, constraints) {
-          double barHeight = constraints.maxHeight * 0.8;
-
-          return Container(
-            height: barHeight,
-            width: 40,
-            decoration: BoxDecoration(
-              color: Colors.grey.shade800,
-              borderRadius: const BorderRadius.only(
-                topRight: Radius.circular(15),
-                topLeft: Radius.circular(15),
-              ),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Container(
-                  height: progressValue * barHeight,
-                  constraints:
-                      BoxConstraints(maxHeight: barHeight, minHeight: 0),
-                  decoration: BoxDecoration(
-                    color: Colors.purpleAccent.shade400,
-                    borderRadius: const BorderRadius.only(
-                      topRight: Radius.circular(12),
-                      topLeft: Radius.circular(12),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          );
-        },
+  Widget _gauge({required double barHeight, required double value}) {
+    return Container(
+      height: value * barHeight,
+      constraints: BoxConstraints(maxHeight: barHeight, minHeight: 0),
+      decoration: BoxDecoration(
+        color: Colors.purpleAccent.shade400,
+        borderRadius: const BorderRadius.only(
+          topRight: Radius.circular(12),
+          topLeft: Radius.circular(12),
+        ),
       ),
     );
   }
 
-  Widget _buildScore() {
+  Widget _gaugeBar() {
+    return Align(
+      alignment: Alignment.bottomRight,
+      child: ValueListenableBuilder(
+        valueListenable: gauge,
+        builder: (context, value, _) => LayoutBuilder(
+          builder: (context, constraints) {
+            double barHeight = constraints.maxHeight * 0.8;
+            return Container(
+              height: barHeight,
+              width: 40,
+              decoration: BoxDecoration(
+                color: Colors.grey.shade800,
+                borderRadius: const BorderRadius.only(
+                  topRight: Radius.circular(15),
+                  topLeft: Radius.circular(15),
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [_gauge(barHeight: barHeight, value: value)],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _score() {
     return Center(
       child: Column(
         children: [
@@ -135,11 +149,11 @@ class _MyHighScoreState extends State<MyHighScore> {
       body: SafeArea(
         child: Column(
           children: [
-            _buildScore(),
+            _score(),
             Expanded(
               child: Padding(
-                padding: const EdgeInsets.only(left: 20, right: 24, bottom: 80),
-                child: _buildProgressBar(),
+                padding: const EdgeInsets.fromLTRB(20, 0, 24, 80),
+                child: _gaugeBar(),
               ),
             ),
           ],
