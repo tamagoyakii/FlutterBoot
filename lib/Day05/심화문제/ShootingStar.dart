a8196a5eca267065e4a0ac98cc0889f9c6735e66
tamagoyakii <rlawlgus4063@naver.com>
2023-12-01 21:02:54 +0900

    [refactor]

--- lib/Day05/심화문제/ShootingStar.dart
+++ lib/Day05/심화문제/ShootingStar.dart
@@ -26,14 +26,25 @@ class ShootingStar extends StatefulWidget {
 
 class _ShootingStarState extends State<ShootingStar>
     with TickerProviderStateMixin {
-  late final AnimationController _scaleController = AnimationController(
-    duration: const Duration(milliseconds: 60),
-    reverseDuration: const Duration(milliseconds: 60),
-    vsync: this,
-  );
-
+  late final AnimationController _scaleController;
+  late final Animation<double> _scaleAnimation;
+  final List<Widget> _stars = [];
   int _count = 0;
-  List<Widget> stars = [];
+
+  @override
+  void initState() {
+    _scaleController = AnimationController(
+      duration: const Duration(milliseconds: 60),
+      vsync: this,
+    );
+    _scaleAnimation = Tween<double>(begin: 1.0, end: 1.2).animate(
+      CurvedAnimation(
+        parent: _scaleController,
+        curve: Curves.easeIn,
+      ),
+    );
+    super.initState();
+  }
 
   @override
   void dispose() {
@@ -43,17 +54,19 @@ class _ShootingStarState extends State<ShootingStar>
 
   void _shootStar() {
     setState(() {
-      stars.add(
-        AnimatedStar(
-          onAnimationComplete: () {
-            setState(() {
-              // stars.removeAt(0);
-              _count++;
-              _scaleController.forward().then((_) {
-                _scaleController.reverse();
+      _stars.add(
+        LayoutBuilder(
+          builder: (context, constraints) => AnimatedStar(
+            max: constraints.biggest,
+            onAnimationComplete: () {
+              setState(() {
+                _count++;
+                _scaleController.forward().then((_) {
+                  _scaleController.reverse();
+                });
               });
-            });
-          },
+            },
+          ),
         ),
       );
     });
@@ -61,30 +74,36 @@ class _ShootingStarState extends State<ShootingStar>
 
   Widget _buildShootingCount() {
     return ScaleTransition(
-      scale: Tween<double>(
-        begin: 1.0,
-        end: 1.2,
-      ).animate(
-        CurvedAnimation(
-          parent: _scaleController,
-          curve: Curves.easeIn,
-          reverseCurve: Curves.easeOut,
-        ),
-      ),
+      scale: _scaleAnimation,
       child: Container(
         width: 50,
         height: 50,
-        decoration: const BoxDecoration(
-          color: Colors.blue,
+        decoration: BoxDecoration(
+          color: Colors.green.shade700,
           shape: BoxShape.circle,
         ),
         child: Center(
-          child: Text(
-            '$_count',
-            style: const TextStyle(color: Colors.white),
-          ),
+          child: Text('$_count', style: const TextStyle(color: Colors.white)),
         ),
       ),
+    );
+  }
+
+  Widget _buildObstacle() {
+    return const Center(
+      child: Text(
+        'No matter what widgets are in the middle\nanimation whould not be obscured.',
+        textAlign: TextAlign.center,
+        style: TextStyle(color: Colors.grey),
+      ),
+    );
+  }
+
+  Widget _buildShootingButton() {
+    return ElevatedButton(
+      onPressed: _shootStar,
+      style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+      child: const Text('Shoot Star!', style: TextStyle(color: Colors.white)),
     );
   }
 
@@ -97,29 +116,10 @@ class _ShootingStarState extends State<ShootingStar>
         child: Stack(
           fit: StackFit.expand,
           children: [
-            Positioned(
-              top: 20,
-              left: 20,
-              child: _buildShootingCount(),
-            ),
-            const Positioned.fill(
-              child: Center(
-                child: Text(
-                  'No matter what widgets are in the middle\nanimation whould not be obscured.',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.grey),
-                ),
-              ),
-            ),
-            Positioned(
-              bottom: 20,
-              right: 20,
-              child: ElevatedButton(
-                onPressed: _shootStar,
-                child: const Text('Shoot Star!'),
-              ),
-            ),
-            ...stars,
+            Positioned(top: 20, left: 20, child: _buildShootingCount()),
+            Positioned.fill(child: _buildObstacle()),
+            Positioned(bottom: 20, right: 20, child: _buildShootingButton()),
+            ..._stars,
           ],
         ),
       ),
@@ -129,7 +129,12 @@ class _ShootingStarState extends State<ShootingStar>
 
 class AnimatedStar extends StatefulWidget {
   final VoidCallback onAnimationComplete;
-  const AnimatedStar({super.key, required this.onAnimationComplete});
+  final Size max;
+  const AnimatedStar({
+    super.key,
+    required this.max,
+    required this.onAnimationComplete,
+  });
 
   @override
   State<AnimatedStar> createState() => _AnimatedStarState();
@@ -140,6 +145,9 @@ class _AnimatedStarState extends State<AnimatedStar>
   late final AnimationController _slideController;
   late final AnimationController _scaleController;
   late final AnimationController _rotationController;
+  late final Animation<Rect?> _slideAnimation;
+  late final Animation<double> _scaleAnimation;
+  late final Animation<double> _rotationAnimation;
 
   @override
   void initState() {
@@ -147,26 +155,43 @@ class _AnimatedStarState extends State<AnimatedStar>
 
     _slideController = AnimationController(
       duration: const Duration(milliseconds: 600),
-      reverseDuration: const Duration(milliseconds: 600),
       vsync: this,
     );
     _scaleController = AnimationController(
       duration: const Duration(milliseconds: 300),
-      reverseDuration: const Duration(milliseconds: 300),
       vsync: this,
     );
     _rotationController = AnimationController(
       duration: const Duration(milliseconds: 600),
-      reverseDuration: const Duration(milliseconds: 600),
       vsync: this,
+    );
+    _slideAnimation = RectTween(
+      begin:
+          Rect.fromLTWH(widget.max.width - 80, widget.max.height - 50, 50, 50),
+      end: const Rect.fromLTWH(50, 50, 50, 50),
+    ).animate(
+      CurvedAnimation(
+        parent: _slideController,
+        curve: Curves.linear,
+      ),
+    );
+    _scaleAnimation = Tween(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _scaleController,
+        curve: Curves.easeIn,
+      ),
+    );
+    _rotationAnimation = Tween(begin: 0.0, end: 0.2).animate(
+      CurvedAnimation(
+        parent: _rotationController,
+        curve: Curves.linear,
+      ),
     );
 
     _slideController.forward().whenCompleteOrCancel(() {
       widget.onAnimationComplete();
     });
-    _scaleController.forward().then((_) {
-      _scaleController.reverse();
-    });
+    _scaleController.forward().then((_) => _scaleController.reverse());
     _rotationController.forward();
   }
 
@@ -174,56 +199,30 @@ class _AnimatedStarState extends State<AnimatedStar>
   void dispose() {
     _slideController.dispose();
     _scaleController.dispose();
+    _scaleController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        final Size max = constraints.biggest;
-        return Stack(
-          children: [
-            RelativePositionedTransition(
-              size: max,
-              rect: RectTween(
-                begin: Rect.fromLTWH(max.width - 80, max.height - 50, 50, 50),
-                end: const Rect.fromLTWH(60, 75, 50, 50),
-              ).animate(
-                CurvedAnimation(
-                  parent: _slideController,
-                  curve: Curves.linear,
-                ),
-              ),
-              child: ScaleTransition(
-                scale: Tween(
-                  begin: 0.0,
-                  end: 1.0,
-                ).animate(
-                  CurvedAnimation(
-                    parent: _scaleController,
-                    curve: Curves.easeIn,
-                    reverseCurve: Curves.easeOut,
-                  ),
-                ),
-                child: RotationTransition(
-                  turns: Tween<double>(begin: 0.0, end: 0.5).animate(
-                    CurvedAnimation(
-                      parent: _rotationController,
-                      curve: Curves.linear,
-                    ),
-                  ),
-                  child: const Icon(
-                    Icons.star,
-                    color: Colors.yellow,
-                    size: 100,
-                  ),
-                ),
+    return Stack(
+      children: [
+        RelativePositionedTransition(
+          size: widget.max,
+          rect: _slideAnimation,
+          child: ScaleTransition(
+            scale: _scaleAnimation,
+            child: RotationTransition(
+              turns: _rotationAnimation,
+              child: const Icon(
+                Icons.star,
+                color: Colors.yellow,
+                size: 100,
               ),
             ),
-          ],
-        );
-      },
+          ),
+        ),
+      ],
     );
   }
 }
