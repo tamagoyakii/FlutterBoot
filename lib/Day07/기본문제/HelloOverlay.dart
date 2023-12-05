1724da1a953b7d31fb83187c5ca6beae27a1b295
tamagoyakii <rlawlgus4063@naver.com>
2023-12-05 15:36:43 +0900

    [feat] HelloOverlay

---
+++ lib/Day07/기본문제/HelloOverlay.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'HelloOverlay',
+      debugShowCheckedModeBanner: false,
+      home: HelloOverlay(),
+    );
+  }
+}
+
+class HelloOverlay extends StatefulWidget {
+  const HelloOverlay({super.key});
+
+  @override
+  State<HelloOverlay> createState() => _HelloOverlayState();
+}
+
+class _HelloOverlayState extends State<HelloOverlay> {
+  OverlayEntry? overlayEntry;
+
+  void createOverlay({required LayerLink targetLink}) {
+    removeHighlightOverlay();
+
+    assert(overlayEntry == null);
+
+    overlayEntry = OverlayEntry(
+      builder: (BuildContext context) => Stack(
+        children: [
+          CompositedTransformFollower(
+            offset: const Offset(200, -30),
+            link: targetLink,
+            child: IgnorePointer(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.deepPurple),
+                  borderRadius: BorderRadius.circular(3),
+                ),
+                padding: const EdgeInsets.only(left: 15, right: 15),
+                child: const Text(
+                  '↓ You clicked this 😎',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 15,
+                    decoration: TextDecoration.none,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+    Overlay.of(context, debugRequiredFor: widget).insert(overlayEntry!);
+  }
+
+  void removeHighlightOverlay() {
+    overlayEntry?.remove();
+    overlayEntry?.dispose();
+    overlayEntry = null;
+  }
+
+  @override
+  void dispose() {
+    removeHighlightOverlay();
+    super.dispose();
+  }
+
+  Widget _buildOverlayButton({required String text}) {
+    final LayerLink buttonLayerLink = LayerLink();
+
+    return CompositedTransformTarget(
+      link: buttonLayerLink,
+      child: ElevatedButton(
+        onPressed: () => createOverlay(targetLink: buttonLayerLink),
+        child: Text(text),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Hello Overlay')),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              _buildOverlayButton(text: 'Hello!'),
+              const SizedBox(height: 15),
+              _buildOverlayButton(text: 'Press'),
+              const SizedBox(height: 15),
+              _buildOverlayButton(text: 'any'),
+              const SizedBox(height: 15),
+              _buildOverlayButton(text: 'button!'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
